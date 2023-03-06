af2c70cf8f12a512fa1b6e6c304a2d1faaf12187
Mark Sinkovics <sinkovics.mark@gmail.com>
2023-03-05 21:45:40 -0500

    fix: color of the back button

--- Sudoku/ContentView.swift
+++ Sudoku/ContentView.swift
@@ -80,7 +80,7 @@ struct ContentView: View {
         .navigationViewStyle(StackNavigationViewStyle())
         .navigationBarColor(backgroundColor: .sBackground, tintColor: .sText)
         .toolBarColor(backgroundColor: .sBackground, tintColor: .sText)
-
+        .accentColor(.sText)
     }
 }
 
